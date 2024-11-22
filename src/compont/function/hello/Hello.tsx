0bcb79096e92ab3e4abc4fbffb9062df629e71fb
MaxCosmos <renyh2001@outlook.com>
2024-11-22 20:20:31 +0800

    动态的显示问候

--- src/compont/function/hello/Hello.tsx
+++ src/compont/function/hello/Hello.tsx
@@ -1,9 +1,24 @@
-import "./Hello.css"
+import "./Hello.css";
+import { useState, useEffect } from "react";
+
 export function Hello() {
+    const [greeting, setGreeting] = useState("早上好");
+
+    useEffect(() => {
+        const currentHour = new Date().getHours();
+
+        if (currentHour < 12) {
+            setGreeting("早上好");
+        } else if (currentHour < 18) {
+            setGreeting("下午好");
+        } else {
+            setGreeting("晚上好");
+        }
+    }, []);
+
     return (
         <div className="hello">
-            早上好！MaxCosmos
+            {greeting}！MaxCosmos
         </div>
-    )
+    );
 }
-
